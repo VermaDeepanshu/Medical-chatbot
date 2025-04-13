e286a277ab547c608527167c0e8954b8e1a0a037
VermaDeepanshu <deepanshuabohar@gmail.com>
2025-04-13 11:26:18 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Generative AI Project',  # to setup geneative ai project
+    version = '0.0.0',
+    author = 'Deepanshu',
+    author_email = 'rkdeepanshu1081@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
+
+# setup project
+#find pakages find from init.py
